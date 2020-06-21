800496eddfb00c0e9a250f093408b4eccf89994c
sushant097 <sushant1234gautam@gmail.com>
2020-06-21 19:35:18 +0545

    Delete check.py
    This file has no useful information

--- src/check.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-batch_size = 10
-img_size = (800, 64)
-random_matrix = np.random.random((batch_size, img_size[0], img_size[1]))
-
-print(random_matrix)
-print(random_matrix.shape)
